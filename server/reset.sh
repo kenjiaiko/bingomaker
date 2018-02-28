3529f3de9d2ded9d9b51dcaf2e62ec83236917e6
kenjiaiko <kenjiaiko@gmail.com>
2018-02-28 20:35:16 +0900

    update server

--- server/reset.sh
+++ server/reset.sh
@@ -1,7 +1,11 @@
 rm -rf db/*
+rm -rf result/*
+rm -rf exec/*
 for i in `seq 15`
 do
 python pysql.py
 mv bingo.db db/192.168.$i.254.db
 done
+python pysql.py
+mv bingo.db db/localhost.db
 chmod 606 db/*
